fb05a473eaa59fb6f59938cb693cc47d3c29b3d1
emanuel oviedo <cjo1@live.com>
2022-12-31 00:57:26 -0300

    Se agregó una validación al modificar

--- entitywinform/Form1.cs
+++ entitywinform/Form1.cs
@@ -58,12 +58,18 @@ namespace entitywinform
 
             if (dataGridView1.CurrentRow != null)
             {
-                Usuario userxd = (Usuario)dataGridView1.CurrentRow.DataBoundItem;
-                userxd.Usuario1 = usuarioxd.Text;
-                userxd.Password = passwordxd.Text;
-                contextoxd.Usuario.Update(userxd);
-                dataGridView1.DataSource = contextoxd.Usuario.ToList();
-                contextoxd.SaveChanges();
+                if (!String.IsNullOrEmpty(usuarioxd.Text) && !String.IsNullOrEmpty(passwordxd.Text))
+                {
+                    Usuario userxd = (Usuario)dataGridView1.CurrentRow.DataBoundItem;
+                    userxd.Usuario1 = usuarioxd.Text;
+                    userxd.Password = passwordxd.Text;
+                    contextoxd.Usuario.Update(userxd);
+                    dataGridView1.DataSource = contextoxd.Usuario.ToList();
+                    contextoxd.SaveChanges();
+                } else
+                {
+                    MessageBox.Show("Verifica que ningun campo este en blanco");
+                }
             } else 
             {
                 MessageBox.Show("Selecciona un dato antes de intentar modificar");
